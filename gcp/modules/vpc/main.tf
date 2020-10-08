d0a3ba3e7ec5dcba9f23c430e8720994f80c17b9
John Tucker <john@larkintuckerllc.com>
2020-10-08 19:44:43 -0400

    firewall

--- gcp/modules/vpc/main.tf
+++ gcp/modules/vpc/main.tf
@@ -4,3 +4,32 @@ resource "google_compute_network" "this" {
   name                    = var.identifier
   routing_mode            = "REGIONAL"
 }
+
+resource "google_compute_route" "default-route-public" {
+  description      = "Default route to the Internet."
+  dest_range       = "0.0.0.0/0"
+  name             = "default-route-public"
+  network          = google_compute_network.this.name
+  next_hop_gateway = "default-internet-gateway"
+  priority         = 1000
+  tags = [
+    "public"
+  ]
+}
+
+resource "google_compute_firewall" "default-allow-ssh-bastion" {
+  allow {
+    ports = [
+      "22"
+    ]
+    protocol = "tcp"
+  }
+  name     = "default-allow-ssh-bastion"
+  network  = google_compute_network.this.name
+  priority = 65534
+  target_tags = [
+    "bastion"
+  ]
+}
+
+# TODO: FIREWALL INTERNAL
